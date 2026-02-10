3e2b9d96602a56775a6305c6c982786cedc271cb
Siddiq <siddiqahmed882@gmail.com>
2026-02-10 23:10:32 +0500

    feat: add Clarity tracking script and replace SVG icons with components in Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,6 +73,17 @@ export default function RootLayout({
             `,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "vfarlt4d5w");
+            `,
+          }}
+        ></script>
         <ThemeProvider attribute='class' defaultTheme='light' disableTransitionOnChange enableColorScheme>
           <Header />
           <main>{children}</main>
